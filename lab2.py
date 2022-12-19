a6dfbb214b35403035b6b40e175d5755560dbb28
dumdum11-00 <bachpp.m21ict@st.usth.edu.vn>
2022-12-19 18:03:59 +0700

    Lab2 fix did oopsie

--- lab2.py
+++ lab2.py
@@ -2,14 +2,15 @@ import numba
 from numba import cuda
 from numba.cuda.cudadrv import enums
 
-print(numba.cuda.detect())
+# print(numba.cuda.detect())
 
 device = cuda.select_device(0)
+print(device.name)
 
 attribs= [name.replace("CU_DEVICE_ATTRIBUTE_", "") for name in dir(enums) if name.startswith("CU_DEVICE_ATTRIBUTE_")]
 for attr in attribs:
-    if attr == "CLOCK_RATE" and attr == "MULTIPROCESSOR_COUNT" and attr == "COMPUTE_CAPABILITY_MAJOR" and attr == "COMPUTE_CAPABILITY_MINOR":
-        print(attr, '=', getattr(device, attr))
+    if attr == "CLOCK_RATE" or attr == "MULTIPROCESSOR_COUNT" or attr == "COMPUTE_CAPABILITY_MAJOR" or attr == "COMPUTE_CAPABILITY_MINOR":
+        print(attr, '=', getattr(device, attr))        
 device_memory =device.get_primary_context().get_memory_info()
 
 
